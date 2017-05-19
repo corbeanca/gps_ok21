5fa7b7cd9ccd50a28cf4f5abd35876a471b04d18
corbeanca <alexandrubarbu93@gmail.com>
2017-05-19 17:35:02 +0300

    popup functional patial rename clase working matrix

--- Preferences.java
+++ Preferences.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,254 +19,214 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Preferences extends Activity
+public class Preferences extends AppCompatActivity
        implements AdapterView.OnItemClickListener
 
-{   RadioGroup rg_batt;
+{
+    RadioGroup rg_batt;
     RadioButton rb;
     ListView list_color;
     ListView list_shape;
-    SeekBar  seek_acc;
-    SeekBar  seek_radius;
+    SeekBar seek_acc;
+    SeekBar seek_radius;
+    SeekBar seek_pinsize;
+    ImageView pinView;
+//    final boolean batt = true;
 
-    static boolean batt;
-    int pos=2;
-    int sh=1;
-    int col=1;
-    ImageView pinView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-        rg_batt = (RadioGroup) findViewById(R.id.rg_bat);
-//        try{
-//        rb = (RadioButton) findViewById(checkBatt());}
-//        catch (NullPointerException e){}
+        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.fragment_pref);
-        String colors[]= getResources().getStringArray(R.array.online_color);
-        String shapes[]= getResources().getStringArray(R.array.online_shape);
+        rg_batt = (RadioGroup) findViewById(R.id.rg_bat);
+        String colors[] = getResources().getStringArray(R.array.online_color);
+        String shapes[] = getResources().getStringArray(R.array.online_shape);
 
-        pinView =(ImageView)findViewById(R.id.pin_view);
+        pinView = (ImageView) findViewById(R.id.pin_view);
         //vvv    pin-ul setat deja    vvv
-        checkPin();
-       // pinView.setImageResource();
+      //  checkPin();
+        // pinView.setImageResource();
+        checkBatt();
 
-        list_shape= (ListView) findViewById(R.id.list_shapes);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,shapes);
+
+        list_shape = (ListView) findViewById(R.id.list_shapes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shapes);
         list_shape.setAdapter(adapter);
         list_shape.setOnItemClickListener(this);
-        list_color= (ListView) findViewById(R.id.list_color);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,colors);
+        list_color = (ListView) findViewById(R.id.list_color);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, colors);
         list_color.setAdapter(adapter2);
         list_color.setOnItemClickListener(this);
 
-        seek_acc= (SeekBar) findViewById(R.id.seekBar_acc);
-        seek_acc.setProgress(show_seek(1));
-        seek_radius= (SeekBar) findViewById(R.id.seekBar_radius);
-        seekbar(1);
-        seekbar(2);
+        seek_acc = (SeekBar) findViewById(R.id.seekBar_acc);
+        seek_radius = (SeekBar) findViewById(R.id.seekBar_radius);
+        seek_pinsize = (SeekBar) findViewById(R.id.seekBar_size);
+        seekbar("default_accuracy");
+        seekbar("default_radius");
+        seekbar("default_pinsize");
         Button save = (Button) findViewById(R.id.pref_save);
         save.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
-                                        startActivity(new Intent(Preferences.this,Principal.class));
+                                        startActivity(new Intent(Preferences.this, Principal.class));
                                     }
                                 }
         );
 
     }
+    public void checkBatt() {
+        int opt = ShowPref("Battery");
+        if (opt ==1)
+            rb = (RadioButton) findViewById(R.id.but_eco) ;
 
-    private void checkPin() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        String def ;
-        def = sp.getString("pin_shape", null);
-        if (def !=null){
+        else rb = (RadioButton) findViewById(R.id.but_off) ;
 
-        }
+            rb.setChecked(true);
+
     }
-    public int checkBatt(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        int c = sp.getInt("Battery",-1);
-        if (c==0)
-            return 0;
-        else if (c==1)
-            return 1;
 
-        return -1;
-    }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         TextView temp = (TextView) view;
-       int p = position +1;
-
-        Toast.makeText(this,temp.getText(),Toast.LENGTH_SHORT).show();
+        int p = position + 1;
+        int col=4;
+        Toast.makeText(this, temp.getText(), Toast.LENGTH_SHORT).show();
 
         if (parent == list_shape) {
             switch (p) {
                 case 1:
                     pinView.setImageResource(R.drawable.circle);
-                    savePin("circle",1);
+                    //SavePref("model", 1);
+                    col=4;
                     break;
                 case 2:
                     pinView.setImageResource(R.drawable.square);
-                    savePin("square",1);
+                    //SavePref("model", 2);
+                    col=8;
                     break;
-            case 3:
-                pinView.setImageResource(R.drawable.shield);
-                savePin("shield",1);
-                break;
-                            }
-        }
-        else switch (p){
+                case 3:
+                    pinView.setImageResource(R.drawable.shield);
+                    //SavePref("model", 3);
+                    col=12;
+                    break;
+            }
+        } else switch (p) {
             case 1:
                 pinView.setBackgroundColor(Color.RED);
-                savePin("red",0);
+                SavePref("model", col-4);
                 break;
             case 2:
                 pinView.setBackgroundColor(Color.GREEN);
-                savePin("green",0);
+                SavePref("model", col-3);
                 break;
             case 3:
                 pinView.setBackgroundColor(Color.BLUE);
-                savePin("blue",0);
+                SavePref("model", col-2);
                 break;
             case 4:
                 pinView.setBackgroundColor(Color.YELLOW);
-                savePin("yellow",0);
+                SavePref("model", col-1);
                 break;
         }
 
     }
 
-    public void savePin(String save,int i){
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        if(i ==1 )
-        editor.putString("pin_shape", save);
+    public void select_batt(View v) {
+        int radiobuttonid = rg_batt.getCheckedRadioButtonId();
+        rb = (RadioButton) findViewById(radiobuttonid);
+        String a = rb.getText().toString();
+        String c1= "Battery Saving Mode";
+        String c2= "Mod eco";
+        if (a.equals(c1)){
+            Toast.makeText(getApplicationContext(), a, Toast.LENGTH_LONG).show();
+        SavePref("Battery",1);}
+            else if ( a.equals(c2)){
+            Toast.makeText(getApplicationContext(), a, Toast.LENGTH_LONG).show();
+        SavePref("Battery",1);}
         else
-        editor.putString("pin_color", save);
+        {Toast.makeText(getApplicationContext(), a, Toast.LENGTH_LONG).show();
+        SavePref("Battery",0);}
+    }
+
+    public void seekbar(final String that) {
+    switch (that){
+        case  "default_accuracy":{
+            seek_acc.setProgress(ShowPref(that));
+            seek_acc.setOnSeekBarChangeListener(
+                    new SeekBar.OnSeekBarChangeListener() {
+                        int progress_value;
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                            progress_value = progress;
+                        }
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+                            //  Toast.makeText(Preferences.this,"SeekBar sus",Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+//                        text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
+                            Toast.makeText(Preferences.this, "Default accuracy updated", Toast.LENGTH_LONG).show();
+                            SavePref("default_accuracy", progress_value);
+                        }
+                    }
+            );}
+        case  "default_radius": {
+            seek_radius.setProgress(ShowPref(that));
+            seek_radius.setOnSeekBarChangeListener(
+                    new SeekBar.OnSeekBarChangeListener() {
+                        int progress_value;
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                            progress_value = progress;
+                        }
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+                            // Toast.makeText(Preferences.this,"SeekBar jos",Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+//                        text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
+                            Toast.makeText(Preferences.this, "Default radius updated", Toast.LENGTH_LONG).show();
+                            SavePref("default_radius", progress_value);
+                        }
+                    }
+            );}
+        case  "default_pinsize": {
+            seek_pinsize.setProgress(ShowPref(that));
+            seek_pinsize.setOnSeekBarChangeListener(
+                    new SeekBar.OnSeekBarChangeListener() {
+                        int progress_value;
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                            progress_value = progress;
+                        }
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+                            // Toast.makeText(Preferences.this,"SeekBar jos",Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+//                        text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
+                            Toast.makeText(Preferences.this, "Default Size updated", Toast.LENGTH_LONG).show();
+                            SavePref("default_pinsize", progress_value);
+                        }
+                    }
+            );
+        }
+    }
+    }
+
+    public void SavePref(String key, int value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
         editor.commit();
     }
 
-    public String pinXML(String shape, String color){
-        switch(shape){
-            case "circle":
-                switch (color){
-                    case "red": return "circle_red.xml";
-                    case "green": return "circle_green";
-                    case "blue": return "circle_blue";
-                    case "yellow": return "circle_yellow";
-                }
-            case "square":
-                switch (color){
-                    case "red": return "square_red.xml";
-                    case "green": return "square_green";
-                    case "blue": return "square_blue";
-                    case "yellow": return "square_yellow";
-                }
-            case "shield":
-                switch (color) {
-                    case "red":
-                        return "shield_red.xml";
-                    case "green":
-                        return "shield_green";
-                    case "blue":
-                        return "shield_blue";
-                    case "yellow":
-                        return "shield_yellow";
-                }
-        }
-        return null;
-    }
-
-
-
-
-    public void seekbar (final int that){
-
-        if (that == 1){
-       // seek_acc.setProgress(show_seek(that));
-
-        seek_acc.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-
-                    int progress_value;
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        progress_value = progress;
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-                      //  Toast.makeText(Preferences.this,"SeekBar sus",Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-//                        text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
-                        Toast.makeText(Preferences.this,"SeekBar salvat sus",Toast.LENGTH_LONG).show();
-                        saveSeek(that,progress_value);
-                    }
-                }
-        );}
-        else  {      seek_radius.setProgress(show_seek(that));
-        seek_radius.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-
-                    int progress_value;
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                            progress_value = progress;
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-                       // Toast.makeText(Preferences.this,"SeekBar jos",Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-//                        text_view.setText("Covered : " + progress_value + " / " +seek_bar.getMax());
-                        Toast.makeText(Preferences.this,"SeekBar salvat jos",Toast.LENGTH_LONG).show();
-                        saveSeek(that,progress_value);
-                    }
-                }
-        );}
-    }
-
-    public void saveSeek(int that, int progress_value) {
+    public int ShowPref(String key){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor ed = sp.edit();
-        if (that == 1)
-           ed.putInt("default_accuracy", progress_value);
-        else ed.putInt("default_radius", progress_value);
-        ed.commit();
-    }
-
-    public void setBatt(int x)
-    {   SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor ed = sp.edit();
-        if (x==1)
-            ed.putInt("Battery", 1);
-        else ed.putInt("Battery",0);
-        ed.commit();
-
-    }
-
-    public int show_seek( int i) {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        if (i ==1 )
-            return i = sp.getInt("default_accuracy", 3) ;
-        else return i = sp.getInt("default_radius", 3);
-
-
+        return sp.getInt(key,1);
     }
 
 }
-
-
-
-
-
-
-
-
